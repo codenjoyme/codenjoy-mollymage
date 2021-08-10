ef8e2c0fbf4f112eeeb26cbe170f20b9197074b9
Pavlo Reshetylo <pavlo_reshetylo@epam.com>
2021-08-10 13:21:31 +0300

    [mollymage] refactoring BoomEngine

--- src/main/java/com/codenjoy/dojo/mollymage/model/items/blast/BoomEngineOriginal.java
+++ src/main/java/com/codenjoy/dojo/mollymage/model/items/blast/BoomEngineOriginal.java
@@ -30,7 +30,6 @@ import com.codenjoy.dojo.services.Point;
 
 import java.util.LinkedList;
 import java.util.List;
-import static com.codenjoy.dojo.services.PointImpl.*;
 
 public class BoomEngineOriginal implements BoomEngine {
 
@@ -44,39 +43,15 @@ public class BoomEngineOriginal implements BoomEngine {
     public List<Blast> boom(List<? extends Point> barriers, int boardSize, Point source, int radius) {
         List<Blast> blasts = new LinkedList<>();
 
-        add(barriers, boardSize, blasts, source.getX(), source.getY());
+        add(barriers, boardSize, blasts, source);
 
-        for (int dx = 1; dx <= radius; dx++) {
-            int x = source.getX() + dx;
-            int y = source.getY() + 0;
-            if (!add(barriers, boardSize, blasts, x, y)) {
-                break;
-            }
-        }
-
-        for (int dx = -1; dx >= -radius; dx--) {
-            int x = source.getX() + dx;
-            int y = source.getY() + 0;
-            if (!add(barriers, boardSize, blasts, x, y)) {
-                break;
-            }
-        }
-
-        for (int dy = 1; dy <= radius; dy++) {
-            int x = source.getX() + 0;
-            int y = source.getY() + dy;
-
-            if (!add(barriers, boardSize, blasts, x, y)) {
-                break;
-            }
-        }
-
-        for (int dy = -1; dy >= -radius; dy--) {
-            int x = source.getX() + 0;
-            int y = source.getY() + dy;
-
-            if (!add(barriers, boardSize, blasts, x, y)) {
-                break;
+        for (Direction direction : Direction.getValues()) {
+            Point point = source.copy();
+            for (int i = 0; i < radius; i++) {
+                point = direction.change(point);
+                if (!add(barriers, boardSize, blasts, point)) {
+                    break;
+                }
             }
         }
 
@@ -89,32 +64,34 @@ public class BoomEngineOriginal implements BoomEngine {
 
         final int length = poison.getPower();
         final Direction direction = poison.getDirection();
-        Point point = hero;
+        Point point = hero.copy();
         for (int i = 0; i < length; i++) {
             point = direction.change(point);
-            if (!add(barriers, size, blasts, point.getX(), point.getY())) {
+            if (!add(barriers, size, blasts, point)) {
                 break;
             }
         }
         return blasts;
     }
 
-    private boolean add(List<? extends Point> barriers, int boardSize, List<Blast> blasts, int x, int y) {
-        Point pt = pt(x, y);
-
+    private boolean add(List<? extends Point> barriers, int boardSize, List<Blast> blasts, Point pt) {
         if (pt.isOutOf(boardSize)) {
             return false;
         }
 
         if (barriers.contains(pt)) {
-            if (!barriers.get(barriers.indexOf(pt)).getClass().equals(Wall.class)) {    // TODO немного жвачка
-                blasts.add(new Blast(x, y, hero));
+            if (isNotWall(barriers, pt)) {
+                blasts.add(new Blast(pt.getX(), pt.getY(), hero));
             }
             return false;
         }
 
-        blasts.add(new Blast(x, y, hero));
+        blasts.add(new Blast(pt.getX(), pt.getY(), hero));
         return true;
+    }
+
+    private boolean isNotWall(List<? extends Point> barriers, Point pt) {
+        return !barriers.get(barriers.indexOf(pt)).getClass().equals(Wall.class);
     }
 
 }

1d10eb6069e24a8433bd23bcd02bdbf27533b691
ilona illiashenko <ilona.illiashenko@gmail.com>
2023-06-25 22:10:14 +0200

    Fixed game freezing when hero uses POTION_EXPLODER

--- src/main/java/com/codenjoy/dojo/mollymage/model/MollyMage.java
+++ src/main/java/com/codenjoy/dojo/mollymage/model/MollyMage.java
@@ -201,7 +201,10 @@ public class MollyMage extends RoundField<Player, Hero> implements Field {
             if (!owner.isActiveAndAlive()) {
                 if (potion.isOnRemote()) {
                     potion.activateRemote(owner);
-                    owner.getPerk(Element.POTION_REMOTE_CONTROL).decrease();
+                    Perk perk = owner.getPerk(Element.POTION_REMOTE_CONTROL);
+                    if (perk != null) {
+                        perk.decrease();
+                    }
                 }
             }
         }
